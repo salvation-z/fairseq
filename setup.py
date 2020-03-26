2f30d4508307d2dc113eb277fa952b9a0d775aba
salvation-z <15619026195@163.com>
2020-03-26 11:26:00 +0800

    check 4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 # ZZC Version of fairseq
-# commit check 2
+# commit check 4
 
 import os
 from setuptools import setup, find_packages, Extension
